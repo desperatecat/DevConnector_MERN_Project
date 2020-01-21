3117e6965d819004c5bce94d1b6a143e19806f6e
desperatecat <376198935@qq.com>
2020-01-21 12:40:29 -0500

    Profile Field Validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+//load validation
+const validateProfileInput = require("../../validation/profile");
+
 //load profile model
 const Profile = require("../../models/Profile");
 
@@ -23,6 +26,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -41,6 +45,14 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    //check validation
+    if (!isValid) {
+      //return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     //get fields
     const profileFields = {};
     profileFields.user = req.user.id;
