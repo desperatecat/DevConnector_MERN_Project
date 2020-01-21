66548347617aec24cb144067f4dbe4d3dd17f020
desperatecat <376198935@qq.com>
2020-01-20 20:26:26 -0500

    generate jwt tokens

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://Lucas:900812@cluster0-neone.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://Lucas:900812@cluster0-neone.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
