72d1847e213e15c7cf7e6856c36ee0dd5ac4a442
desperatecat <376198935@qq.com>
2020-01-29 14:24:28 -0500

    remove like

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -92,3 +92,16 @@ export const addLike = id => dispatch => {
       })
     );
 };
+
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
