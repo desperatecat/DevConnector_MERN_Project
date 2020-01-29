555edc052219e0f04ef0c3fea642d412f7fb95bd
desperatecat <376198935@qq.com>
2020-01-29 13:09:13 -0500

    add posts route

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 
 //check for token
 if (localStorage.jwtToken) {
@@ -92,6 +93,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
             </div>
             <Footer />
