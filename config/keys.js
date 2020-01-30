ce395123fedb388e9628e7245ba3a38be5e74ca3
desperatecat <376198935@qq.com>
2020-01-30 18:25:06 -0500

    setup keys for prod and dev

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://LucasXu:900930@cluster0-kfjrk.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://LucasXu:900930@cluster0-kfjrk.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
