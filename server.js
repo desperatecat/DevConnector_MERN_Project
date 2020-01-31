e23c4122613b7806c92fdfae6ce380dc74d2e0a7
desperatecat <376198935@qq.com>
2020-01-30 19:41:26 -0500

    remove host from server

--- server.js
+++ server.js
@@ -45,6 +45,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const port = process.env.PORT || 5000;
-const host = "0.0.0.0";
 
-app.listen(port, host, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
